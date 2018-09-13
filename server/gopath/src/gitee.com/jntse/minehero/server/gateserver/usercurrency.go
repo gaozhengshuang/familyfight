d90b8b885a76ab2796308e04418883b68cb2b82e
Franky <liushuaigeq@163.com>
2018-09-13 16:10:38 +0800

    new server

---
+++ server/gopath/src/gitee.com/jntse/minehero/server/gateserver/usercurrency.go
@@ -0,0 +1,89 @@
+package main
+import (
+	"gitee.com/jntse/gotoolkit/log"
+	"gitee.com/jntse/gotoolkit/util"
+	_"gitee.com/jntse/gotoolkit/eventqueue"
+	"gitee.com/jntse/gotoolkit/net"
+	"gitee.com/jntse/minehero/server/tbl"
+	"gitee.com/jntse/minehero/server/def"
+	"gitee.com/jntse/minehero/pbmsg"
+	pb "github.com/golang/protobuf/proto"
+	"encoding/json"
+	"net/http"
+	"strconv"
+	"fmt"
+	"time"
+	"strings"
+)
+
+// money
+func (this *GateUser) GetMoney() uint32   { return this.money }
+func (this *GateUser) AddMoney(gold uint32, reason string) {
+	this.money = this.GetMoney() + gold
+	log.Info("玩家[%d] 添加金币[%d] 库存[%d] 原因[%s]", this.Id(), gold, this.GetMoney(), reason)
+}
+
+// gold
+func (this *GateUser) GetGold() uint64 { return this.gold }
+func (this *GateUser) AddGold(gold uint64, reason string) {
+	this.gold = this.GetGold() + gold
+	log.Info("玩家[%d] 添加gold[%d] 库存[%d] 原因[%s]", this.Id(), gold, this.GetGold(), reason)
+	send := &msg.GW2C_UpdateGold{Num:pb.Uint32(this.GetGold())}
+	this.SendMsg(send)
+}
+func (this *GateUser) RemoveGold(gold uint64, reason string ) bool {
+	if this.GetGold() < gold {
+		return false
+	}
+	this.gold = this.GetGold() - gold
+	log.Info("玩家[%d] 扣除gold[%d] 库存[%d] 原因[%s]", this.Id(), gold, this.GetGold(), reason)
+	send := &msg.GW2C_UpdateGold{Num:pb.Uint32(this.GetGold())}
+	this.SendMsg(send)
+}
+
+// 添加元宝
+func (this *GateUser) GetYuanbao() uint32 { return this.yuanbao }
+func (this *GateUser) AddYuanbao(yuanbao uint32, reason string) {
+	this.yuanbao = this.GetYuanbao() + yuanbao
+	send := &msg.GW2C_UpdateYuanbao{Num:pb.Uint32(this.GetYuanbao())}
+	this.SendMsg(send)
+	RCounter().IncrByDate("item_add", uint32(msg.ItemId_YuanBao), yuanbao)
+	log.Info("玩家[%d] 添加元宝[%d] 库存[%d] 原因[%s]", this.Id(), yuanbao, this.GetYuanbao(), reason)
+	this.PlatformPushLootMoney(float32(yuanbao))
+}
+func (this *GateUser) RemoveYuanbao(yuanbao uint32, reason string) bool {
+	if this.GetYuanbao() >= yuanbao {
+		this.yuanbao = this.GetYuanbao() - yuanbao
+		send := &msg.GW2C_UpdateYuanbao{Num:pb.Uint32(this.GetYuanbao())}
+		this.SendMsg(send)
+		log.Info("玩家[%d] 扣除元宝[%d] 库存[%d] 原因[%s]", this.Id(), yuanbao, this.GetYuanbao(), reason)
+		RCounter().IncrByDate("item_remove", uint32(msg.ItemId_YuanBao), yuanbao)
+		this.PlatformPushConsumeMoney(float32(yuanbao))
+		return true
+	}
+	log.Info("玩家[%d] 扣除元宝[%d]失败 库存[%d] 原因[%s]", this.Id(), yuanbao, this.GetYuanbao(), reason)
+	return false
+}
+
+
+// 添加金卷
+func (this *GateUser) GetCoupon() uint32  { return this.coupon }
+func (this *GateUser) AddCoupon(num uint32, reason string) {
+	this.coupon = this.GetCoupon() + num
+	send := &msg.GW2C_UpdateCoupon{Num:pb.Uint32(this.GetCoupon())}
+	this.SendMsg(send)
+	log.Info("玩家[%d] 添加金卷[%d] 库存[%d] 原因[%s]", this.Id(), num, this.GetCoupon(), reason)
+}
+func (this *GateUser) RemoveCoupon(num uint32, reason string) bool {
+	if this.GetCoupon() >= num {
+		this.coupon = this.GetCoupon() - num
+		send := &msg.GW2C_UpdateCoupon{Num:pb.Uint32(this.GetCoupon())}
+		this.SendMsg(send)
+		log.Info("玩家[%d] 添加金卷[%d] 库存[%d] 原因[%s]", this.Id(), num, this.GetCoupon(), reason)
+		//CountMgr().AddRemove(uint32(msg.ItemId_Coupon), num)
+		RCounter().IncrByDate("item_remove", uint32(msg.ItemId_Coupon), num)
+		return true
+	}
+	log.Info("玩家[%d] 添加金卷[%d]失败 库存[%d] 原因[%s]", this.Id(), num, this.GetCoupon(), reason)
+	return false
+}
--- server/gopath/src/gitee.com/jntse/minehero/server/gateserver/usermaid.go
+++ server/gopath/src/gitee.com/jntse/minehero/server/gateserver/usermaid.go
@@ -2,18 +2,12 @@ package main
 import (
 	"gitee.com/jntse/gotoolkit/log"
 	"gitee.com/jntse/gotoolkit/util"
-	_"gitee.com/jntse/gotoolkit/eventqueue"
-	"gitee.com/jntse/gotoolkit/net"
 	"gitee.com/jntse/minehero/server/tbl"
 	"gitee.com/jntse/minehero/server/def"
 	"gitee.com/jntse/minehero/pbmsg"
 	pb "github.com/golang/protobuf/proto"
 	"encoding/json"
-	"net/http"
-	"strconv"
-	"crypto/md5"
 	"fmt"
-	"time"
 	"strings"
 )
 
@@ -24,47 +18,92 @@ type MaidData struct {
 	id				uint32
 	count 			uint32
 }
+
+func (this *MaidData) PackBin() *msg.MaidData{
+	data := &msg.MaidData{}
+	data.Id = pb.Uint32(this.id)
+	data.Count = pb.Uint32(this.count)
+	return data
+}
+
+// --------------------------------------------------------------------------
+/// @brief 侍女商店
+// --------------------------------------------------------------------------
+type MaidShop struct {
+	id 				uint32
+	price 			uint64
+}
+
+func (this *MaidShop) PackBin() *msg.MaidShopData{
+	data := &msg.MaidShopData{}
+	data.Id = pb.Uint32(this.id)
+	data.Price = pb.Uint64(this.price)
+	return data
+}
+
 // --------------------------------------------------------------------------
 /// @brief 玩家侍女
 // --------------------------------------------------------------------------
 type UserMaid struct {
 	maids			map[uint32]* MaidData
+	maxid 			uint32
+	shop 			map[uint32]* MaidShop
 }
 
-// 添加侍女
-func (this *UserMaid) AddMaid(id uint32, count uint32) *MaidData {
-	maid, ok := this.maids[id];
-	if !ok {
-		maid = &MaidData{}
-		maid.id = id
-		maid.count = 0
-		this.maids[id] = maid
+func (this *UserMaid) Init() {
+	this.maxid = 0
+	this.maids = make(map[uint32]*MaidData)
+	this.shop = make(map[uint32]*MaidShop)
+}
+//加载数据
+func (this *UserMaid) LoadBin(user *GateUser,bin *msg.Serialize) {
+	maidbin := bin.GetMaid();
+	if maidbin == nil { return }
+	this.maxid = maidbin.GetMaxid()
+	for _, data := range maidbin.GetDatas() {
+		this.AddMaid(user,data.GetId(),data.GetCount())
 	}
-	maid.count = maid.count + count
-	return maid
+	for _, data := range maidbin.GetShop() {
+		shop := &MaidShop{}
+		shop.id = data.GetId()
+		shop.price = data.GetPrice()
+		this.shop[data.GetId()] = shop
+	}
 }
 
-// 减少侍女
-func (this *UserMaid) RemoveMaid(id uint32,count uint32) *MaidData {
-	maid, ok := this.maids[id];
-	if !ok {
-		return nil
+func (this *UserMaid) PackBin(bin *msg.Serialize) {
+	bin.Maid = &msg.MaidBin{Datas:make([]*msg.MaidData,0),Shop:make([]*msg.MaidShopData,0)}
+	maidbin := bin.GetMaid()
+	maidbin.Maxid = pb.Uint32(this.maxid)
+	for _, maid := range this.maids {
+		maidbin.Datas = append(maidbin.Datas,maid.PackBin())
 	}
-	if maid.count < count {
-		return nil
+	for _, shop := range this.shop {
+		maidbin.Shop = append(maidbin.Shop,shop.PackBin())
 	}
-	maid.count = maid.count - count
-	return maid
 }
-
 // ========================= 消息接口 ========================= 
 //购买侍女 
 func (this *UserMaid) BuyMaid(user *GateUser,id uint32) (result uint32 ,addition *MaidData){
-
+	maidconfg, find := tbl.TMaidLevelBase.TMaidLevelById[this.maxid]
+	if !find {
+		return 1,nil
+	}
+	shopdata, find := this.shop[id]
+	if !find {
+		return 2,nil
+	}
+	if user.GetGold() < shopdata.price {
+		return 3,nil
+	}
+	//可以买了
+	maid := this.AddMaid(user,id,1)
+	user.RemoveGold(shopdata.price, "购买侍女")
+	return 0, maid
 }
 
 //合并侍女
-func (this *UserMaid) MergeMaid(id uint32) (result uint32,removed *MaidData, addition *MaidData){
+func (this *UserMaid) MergeMaid(user *GateUser,id uint32) (result uint32,removed *MaidData, addition *MaidData){
 	maidconfg, find := tbl.TMaidLevelBase.TMaidLevelById[id]
 	if !find {
 		return 1,nil,nil
@@ -85,6 +124,75 @@ func (this *UserMaid) MergeMaid(id uint32) (result uint32,removed *MaidData, add
 	if removed == nil {
 		return 5,nil,nil
 	}
-	addition = this.AddMaid(nextid,1)
+	addition = this.AddMaid(user,nextid,1)
 	return 0,removed,addition
+}
+
+// ========================= 数据处理 ========================= 
+// 添加侍女
+func (this *UserMaid) AddMaid(user *GateUser, id uint32, count uint32) *MaidData {
+	maid, ok := this.maids[id];
+	if !ok {
+		maid = &MaidData{}
+		maid.id = id
+		maid.count = 0
+		this.maids[id] = maid
+	}
+	maid.count = maid.count + count
+	if id > this.maxid {
+		this.ChangeMaxId(user,id)
+	}
+	return maid
+}
+
+// 减少侍女
+func (this *UserMaid) RemoveMaid(id uint32,count uint32) *MaidData {
+	maid, ok := this.maids[id];
+	if !ok {
+		return nil
+	}
+	if maid.count < count {
+		return nil
+	}
+	maid.count = maid.count - count
+	return maid
+}
+
+//修改拥有的最高的侍女
+func (this *UserMaid) ChangeMaxId(user *GateUser,id uint32) {
+	if this.maxid >= id {
+		return
+	}
+	maidconfig, find := tbl.TMaidLevelBase.TMaidLevelById[id]
+	if !find {
+		return
+	}
+	newShop := make(map[uint32]*MaidShop)
+	for _, v := range maidconfig.ShopShow {
+		oldshop, find := this.shop[v]
+		if find {
+			//找不到初始化价格
+			maidshop, find := tbs.TMaidShopBase.TMaidShopById[v]
+			price := 0
+			if find {
+				price = maidshop.Price
+			}
+			shop := &MaidShop{}
+			shop.id = v
+			shop.price = price
+			newShop[v] = oldshop
+		} else {
+			// 继承老的价格
+			newShop[oldshop.id] = oldshop
+		}
+	}
+	//修改吧
+	this.maxid = id
+	this.shop = newShop
+
+	send := &GW2C_AckMaidShop{Shop:make([]*msg.MaidShopData,0)}
+	for _, v := range this.shop {
+		send.Shop = append(shend.Shop,v.PackBin())
+	}
+	user.SendMsg(send)
 }
