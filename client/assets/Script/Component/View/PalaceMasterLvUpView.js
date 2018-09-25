41bc46037c380d35752f9a95a5815fb8f5e90338
hongdawei <84318436@qq.com>
2018-09-25 12:19:28 +0800

    宫殿升级界面提交

--- client/assets/Script/Component/View/PalaceMasterLvUpView.js
+++ client/assets/Script/Component/View/PalaceMasterLvUpView.js
@@ -10,8 +10,6 @@ cc.Class({
         label_name: { default:null, type:cc.Label },
         label_detail: { default:null, type:cc.Label },
         label_lvTitle: { default:null, type:cc.Label },
-        label_maidNum: { default:null, type:cc.Label },
-        label_goldNum: { default:null, type:cc.Label },
         label_cardNum: { default:null, type:cc.Label },
     },
 
@@ -37,6 +35,8 @@ cc.Class({
         this._data = Game.PalaceModel.GetPalaceDataById(Game.PalaceModel.GetCurPalaceId());
         let palaceMapBase = Game.ConfigController.GetConfigById("PalaceMap", this._data.id);
         if (palaceMapBase) {
+            Game.ResController.SetSprite(this.image_master, palaceMapBase.Headpath);
+
             let masterLvUpBase = Game.PalaceModel.GetPalaceMasterLvUpBase(palaceMapBase.Master, this._data.level);  //主人升级相关
             if (masterLvUpBase) {
                 let lvItemBase = masterLvUpBase.LevelupCost[0];
@@ -51,11 +51,28 @@ cc.Class({
                         this.label_cardNum.string = '所需卡牌' + Game.ItemModel.GetItemNumById(item_num[0]) + "/" + item_num[1];
                     }
                 }
+
+                this.label_tltie.string = '(' + masterLvUpBase.levelName + ')';
+                let maidBase = Game.ConfigController.GetConfigById("TMaidLevel", palaceMapBase.Master);
+                if (maidBase) {
+                    this.label_name.string = maidBase.Name;
+                    this.label_detail.string = maidBase.RoleDescribe;
+                }
+
+                let nextMasterLvUpBase = Game.PalaceModel.GetPalaceMasterLvUpBase(palaceMapBase.Master, this._data.level + 1); //下个等级数据
+                if (nextMasterLvUpBase) {
+                    this.label_lvTitle.string = masterLvUpBase.levelName + ">>>" + nextMasterLvUpBase.levelName;
+                } else {
+                    this.label_lvTitle.string = masterLvUpBase.levelName;
+                }
             }
         }
     },
 
     onLvUp() {
-
+        Game.NetWorkController.Send('msg.C2GW_ReqMasterLevelup', 
+        {
+            id: Game.PalaceModel.GetCurPalaceId(),
+        });
     },
 });
