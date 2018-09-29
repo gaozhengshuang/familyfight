b4d5a9a858722d6f35990647b063644c500cd745
hongdawei <84318436@qq.com>
2018-09-29 17:01:01 +0800

    第一次开放关卡特效

--- client/assets/Script/Component/Node/PassCellNode.js
+++ client/assets/Script/Component/Node/PassCellNode.js
@@ -5,12 +5,14 @@ cc.Class({
 
     properties: {
         passClickTrue_img: { default: null, type: cc.Sprite },
-        pass_img: { default: null, type: cc.Sprite }
+        pass_img: { default: null, type: cc.Sprite },
+        animation_light: { default: null, type: cc.Animation }
     },
 
     // use this for initialization
     onLoad() {
-
+        this.animation_light.node.active = false;
+        this.animation_light.on('stop', this.onStop, this);
     },
 
     init(index, data, reload, group) {
@@ -19,6 +21,19 @@ cc.Class({
 
         this.passClickTrue_img.node.active = Game.MaidModel.GetCurPass() == this._data.Id;
         Game.ResController.SetSprite(this.pass_img, this._data.Path);
+
+        let _dialoguePass = JSON.parse(cc.sys.localStorage.getItem('dialoguePass'));    //判断关卡第一次开放
+        if ((this._data.Id == Game.MaidModel.GetTopPass() && _dialoguePass.lookPass < this._data.Id)) {
+            let passData = {
+                userid: _dialoguePass.userid,
+                lookPass: this._data.Id,
+                pass: _dialoguePass.pass,
+            };
+            cc.sys.localStorage.setItem('dialoguePass', JSON.stringify(passData));
+
+            this.animation_light.node.active = true;
+            this.animation_light.play("FlashNewPass");
+        }
     },
 
     clicked() {
@@ -32,14 +47,21 @@ cc.Class({
         let _dialoguePass = JSON.parse(cc.sys.localStorage.getItem('dialoguePass'));
         if (this._data.Id == Game.MaidModel.GetTopPass() && _dialoguePass.pass < this._data.Id) {
             if (this._data.DialogueID != 0) {
-                Game.NotificationController.Emit(Game.Define.EVENT_KEY.SHOWDIALOGUE_PLAYER, this._data.DialogueID);
-
                 let passData = {
                     userid: Game.UserModel.GetUserId(),
+                    lookPass: _dialoguePass.lookPass,
                     pass: this._data.Id,
                 };
                 cc.sys.localStorage.setItem('dialoguePass', JSON.stringify(passData));
+                
+                Game.NotificationController.Emit(Game.Define.EVENT_KEY.SHOWDIALOGUE_PLAYER, this._data.DialogueID);
             }
         }
+    },
+
+    onStop(event) {
+        var state = event.detail;    // state instanceof cc.AnimationState
+        var type = event.type;       // type === 'strop';
+        this.animation_light.node.active = false;
     }
 });
