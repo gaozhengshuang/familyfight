11bef25e384acecc2b86cc6ede455a2fbe3da10d
Franky <liushuaigeq@163.com>
2018-09-29 17:37:12 +0800

    update

--- client/assets/Script/Component/Scene/TestScene.js
+++ client/assets/Script/Component/Scene/TestScene.js
@@ -9,12 +9,16 @@ cc.Class({
     onLoad() {
     },
     start() {
-        console.log(parseInt('1'))
-        console.log(parseInt(2))
+        // console.log(parseInt('1'))
+        // console.log(parseInt(2))
 
-        let x = { a: 1, b: 2 };
-        let y = { c: 3, b: 4 };
-        console.log(Game._.merge(x, y));
+        // let x = { a: 1, b: 2 };
+        // let y = { c: 3, b: 4 };
+        // console.log(Game._.merge(x, y));
+
+        let info = 'U2FsdGVkX1+eh7ST1Op6F6ZvuS4UJ8c1GUlRMruFCJlkl1Io8rI7m5nIPCaJSgB4hCAogoD0D/VbUZ6bwUpWsIpBA5PfkRJ0iuuOpsmog8jZVqI/lGeAa3Yux9Bm7EmeRvEUXa1q29O/Fa9IIpJ5kroJYf7Do3qmntc7JG8ylPaMjr1XH1hx0fWMfBPdQbv/5/IWetLRiFH+SITIPDMch16QJ66+YcZP+gTggH8gTKs=';
+        let str = Game.Crypto.AESDecrypt('FitzHMa42EpbzEmR', 'ZwpkH3ecNj8nnS4W', info);
+        console.log(str);
     },
     update(dt) {
     },
--- client/assets/Script/Util/Crypto.js
+++ client/assets/Script/Util/Crypto.js
@@ -1,19 +1,30 @@
 let CryptoJS = require("crypto-js");
-const key = CryptoJS.enc.Utf8.parse('1234123412ABCDEF');    //十六位十六进制数作为密钥
-const iv = CryptoJS.enc.Utf8.parse('ABCDEF1234123412');     //十六位十六进制数作为密钥偏移量
 
 let Crypto = {
-    AESEncrypt: function (word) {
-        let srcs = CryptoJS.enc.Utf8.parse(word);
-        let encrypted = CryptoJS.AES.encrypt(srcs, key, { iv: iv, mode: CryptoJS.mode.CBC, paaing: CryptoJS.pad.Pkcs7 });
-        return encrypted.ciphertext.toString().toUpperCase();
+    AESEncrypt: function (key, iv, word) {
+        // let srcs = CryptoJS.enc.Utf8.parse(word);
+        // let encrypted = CryptoJS.AES.encrypt(srcs, key, { iv: iv, mode: CryptoJS.mode.CBC, paaing: CryptoJS.pad.Pkcs7 });
+        // return encrypted.ciphertext.toString().toUpperCase();
+        var encrypted = CryptoJS.AES.encrypt(data, key, {
+            iv: iv,
+            mode: CryptoJS.mode.CBC,
+            padding: CryptoJS.pad.Pkcs7
+        });
+        return encrypted.toString();
     },
-    AESDecrypt: function (word) {
-        let encryptedHexStr = CryptoJS.enc.Hex.parse(word);
-        let srcs = CryptoJS.enc.Base64.stringify(encryptedHexStr);
-        let decrypt = CryptoJS.AES.decrypt(srcs, key, { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 });
-        let decryptedStr = decrypt.toString(CryptoJS.enc.Utf8);
-        return decryptedStr.toString();
+    AESDecrypt: function (key, iv, word) {
+        var decrypted = CryptoJS.AES.decrypt(word, key, {
+            iv: iv,
+            mode: CryptoJS.mode.CBC,
+            padding: CryptoJS.pad.Pkcs7
+        });
+        decrypted = CryptoJS.enc.Utf8.stringify(decrypted);// 转换为 utf8 字符串
+        return decrypted;
+        // let encryptedHexStr = CryptoJS.enc.Hex.parse(word);
+        // let srcs = CryptoJS.enc.Base64.stringify(encryptedHexStr);
+        // let decrypt = CryptoJS.AES.decrypt(srcs, key, { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 });
+        // let decryptedStr = decrypt.toString(CryptoJS.enc.Utf8);
+        // return decryptedStr.toString();
     }
 }
 
