9f88e216b7d6fb0b67849f4ed9034d6b1db68f1d
hongdawei <84318436@qq.com>
2018-10-08 17:03:27 +0800

    引导初步架构

--- client/assets/Script/Component/View/GuideView.js
+++ client/assets/Script/Component/View/GuideView.js
@@ -4,7 +4,10 @@ cc.Class({
     extends: cc.GameComponent,
 
     properties: {
-       
+        node_dailogbox: { default: null, type: cc.Node },
+        node_guideChild: { default: null, type: cc.Node },
+        node_arrow: { default: null, type: cc.Node },
+        label_dailog: { default: null, type: cc.Label },
     },
 
     onEnable() {
