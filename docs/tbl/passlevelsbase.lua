8940e33970b711bd35d5bb2ea3841df34234502c
Franky <liushuaigeq@163.com>
2018-10-12 16:16:34 +0800

    update

--- docs/tbl/passlevelsbase.lua
+++ docs/tbl/passlevelsbase.lua
@@ -6,7 +6,7 @@ export var PassLevels : table.IPassLevelsDefine[] = [
 		{ Id : 1, Name : "选秀女", Path : "Image/GameScene/Pass/pass_1", NextLevels : 2, DialogueID : 0, MapPrefab : "Prefab/map/map05", ChapterID : 1, ChapterName : "第一章", Index : 1 	},
 		{ Id : 2, Name : "步步生莲", Path : "Image/GameScene/Pass/pass_7", NextLevels : 3, DialogueID : 0, MapPrefab : "Prefab/map/map01", ChapterID : 1, ChapterName : "第一章", Index : 2 	},
 		{ Id : 3, Name : "作弊", Path : "Image/GameScene/Pass/pass_2", NextLevels : 4, DialogueID : 33, MapPrefab : "Prefab/map/map03", ChapterID : 1, ChapterName : "第一章", Index : 3 	},
-		{ Id : 4, Name : "泼水", Path : "Image/GameScene/Pass/pass_3", NextLevels : 5, DialogueID : 7, MapPrefab : "Prefab/map/map02", ChapterID : 1, ChapterName : "第一章", Index : 4 	},
+		{ Id : 4, Name : "泼水", Path : "Image/GameScene/Pass/pass_3", NextLevels : 5, DialogueID : 39, MapPrefab : "Prefab/map/map02", ChapterID : 1, ChapterName : "第一章", Index : 4 	},
 		{ Id : 5, Name : "绣坊比赛", Path : "Image/GameScene/Pass/pass_2", NextLevels : 6, DialogueID : 33, MapPrefab : "Prefab/map/map05", ChapterID : 2, ChapterName : "第二章", Index : 1 	},
 		{ Id : 6, Name : "皇后千秋", Path : "Image/GameScene/Pass/pass_4", NextLevels : 7, DialogueID : 25, MapPrefab : "Prefab/map/map06", ChapterID : 2, ChapterName : "第二章", Index : 2 	},
 		{ Id : 7, Name : "初入长春宫", Path : "Image/GameScene/Pass/pass_5", NextLevels : 8, DialogueID : 25, MapPrefab : "Prefab/map/map04", ChapterID : 2, ChapterName : "第二章", Index : 3 	},
