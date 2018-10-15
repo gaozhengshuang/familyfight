6b412a9125d743d63eabdc93ca31892256d12269
hongdawei <84318436@qq.com>
2018-10-15 13:45:36 +0800

    外网切换成内网

--- client/assets/Script/Platform/CommonGame.js
+++ client/assets/Script/Platform/CommonGame.js
@@ -5,7 +5,7 @@ let NotificationController = require('../Controller/NotificationController');
 let CommonPlatform = {
     PLATFORM : 'Normal',    //'Normal',//'Wechat',//'QQPlay'
     WSPrefix : 'ws://',
-    LoginHost : '210.73.214.72:7020',   //'210.73.214.72:7020' '192.168.30.206:7020'  LoginPort: 17002
+    LoginHost : '192.168.30.206:7020',   //'210.73.214.72:7020' '192.168.30.206:7020'  LoginPort: 17002
     LoginSuffix : 'ws_handler',
     RegisteHost : 'https://dati-service17003.giantfun.cn/',     //'http://192.168.30.202:7003/'
 
