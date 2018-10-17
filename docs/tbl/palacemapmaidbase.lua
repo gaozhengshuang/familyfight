2178cdd4f8df13a19c6c57e192ebde38bf03fb22
Franky <liushuaigeq@163.com>
2018-10-17 16:36:14 +0800

    update

--- docs/tbl/palacemapmaidbase.lua
+++ docs/tbl/palacemapmaidbase.lua
@@ -3,24 +3,24 @@
 
 module table {
 export var PalaceMapMaid : table.IPalaceMapMaidDefine[] = [
-		{ Id : 300, OpenLevel : 1, UnlockPrice : [ "0_1" ], GoldAddition : [ "0_76" ], ItemGroup : [ "10001_1", "20001_1", "20002_1", "20003_1", "20004_1", "20005_1", "20006_1" ], ItemProb : 10000, Describe : "主要产出御膳道具" 	},
-		{ Id : 301, OpenLevel : 2, UnlockPrice : [ "1_95", "0_0000" ], GoldAddition : [ "0_95" ], ItemGroup : [ "30001_1", "30002_1", "30003_1", "30004_1", "30005_1", "30006_1", "30007_1", "30008_1" ], ItemProb : 10000, Describe : "主要产出御品道具" 	},
-		{ Id : 302, OpenLevel : 3, UnlockPrice : [ "1_190", "0_0000" ], GoldAddition : [ "0_190" ], ItemGroup : [ "40001_1", "40002_1", "40003_1", "40004_1", "40005_1" ], ItemProb : 10000, Describe : "主要产出服装道具" 	},
-		{ Id : 303, OpenLevel : 4, UnlockPrice : [ "1_190", "0_0000" ], GoldAddition : [ "0_190" ], ItemGroup : [ "10001_1", "20001_1", "20002_1", "20003_1", "20004_1", "20005_1", "20006_1" ], ItemProb : 10000, Describe : "主要产出御膳道具" 	},
-		{ Id : 304, OpenLevel : 5, UnlockPrice : [ "1_190", "0_0000" ], GoldAddition : [ "0_190" ], ItemGroup : [ "30001_1", "30002_1", "30003_1", "30004_1", "30005_1", "30006_1", "30007_1", "30008_1" ], ItemProb : 10000, Describe : "主要产出御品道具" 	},
-		{ Id : 305, OpenLevel : 6, UnlockPrice : [ "1_190", "0_0000" ], GoldAddition : [ "0_190" ], ItemGroup : [ "40001_1", "40002_1", "40003_1", "40004_1", "40005_1" ], ItemProb : 10000, Describe : "主要产出服装道具" 	},
-		{ Id : 200, OpenLevel : 1, UnlockPrice : [ "1_276", "0_0000" ], GoldAddition : [ "0_276" ], ItemGroup : [ "10001_1", "20001_1", "20002_1", "20003_1", "20004_1", "20005_1", "20006_1" ], ItemProb : 10000, Describe : "主要产出御膳道具" 	},
-		{ Id : 201, OpenLevel : 2, UnlockPrice : [ "1_345", "0_0000" ], GoldAddition : [ "0_345" ], ItemGroup : [ "30001_1", "30002_1", "30003_1", "30004_1", "30005_1", "30006_1", "30007_1", "30008_1" ], ItemProb : 10000, Describe : "主要产出御品道具" 	},
-		{ Id : 202, OpenLevel : 3, UnlockPrice : [ "1_690", "0_0000" ], GoldAddition : [ "0_690" ], ItemGroup : [ "40001_1", "40002_1", "40003_1", "40004_1", "40005_1" ], ItemProb : 10000, Describe : "主要产出服装道具" 	},
-		{ Id : 203, OpenLevel : 4, UnlockPrice : [ "1_690", "0_0000" ], GoldAddition : [ "0_690" ], ItemGroup : [ "10001_1", "20001_1", "20002_1", "20003_1", "20004_1", "20005_1", "20006_1" ], ItemProb : 10000, Describe : "主要产出御膳道具" 	},
-		{ Id : 204, OpenLevel : 5, UnlockPrice : [ "1_690", "0_0000" ], GoldAddition : [ "0_690" ], ItemGroup : [ "30001_1", "30002_1", "30003_1", "30004_1", "30005_1", "30006_1", "30007_1", "30008_1" ], ItemProb : 10000, Describe : "主要产出御品道具" 	},
-		{ Id : 205, OpenLevel : 6, UnlockPrice : [ "1_690", "0_0000" ], GoldAddition : [ "0_690" ], ItemGroup : [ "40001_1", "40002_1", "40003_1", "40004_1", "40005_1" ], ItemProb : 10000, Describe : "主要产出服装道具" 	},
-		{ Id : 100, OpenLevel : 1, UnlockPrice : [ "1_472", "0_0000" ], GoldAddition : [ "0_472" ], ItemGroup : [ "10001_1", "20001_1", "20002_1", "20003_1", "20004_1", "20005_1", "20006_1" ], ItemProb : 10000, Describe : "主要产出御膳道具" 	},
-		{ Id : 101, OpenLevel : 2, UnlockPrice : [ "1_590", "0_0000" ], GoldAddition : [ "0_590" ], ItemGroup : [ "30001_1", "30002_1", "30003_1", "30004_1", "30005_1", "30006_1", "30007_1", "30008_1" ], ItemProb : 10000, Describe : "主要产出御品道具" 	},
-		{ Id : 102, OpenLevel : 3, UnlockPrice : [ "1_1180", "0_0000" ], GoldAddition : [ "0_1180" ], ItemGroup : [ "40001_1", "40002_1", "40003_1", "40004_1", "40005_1" ], ItemProb : 10000, Describe : "主要产出服装道具" 	},
-		{ Id : 103, OpenLevel : 4, UnlockPrice : [ "1_1180", "0_0000" ], GoldAddition : [ "0_1180" ], ItemGroup : [ "10001_1", "20001_1", "20002_1", "20003_1", "20004_1", "20005_1", "20006_1" ], ItemProb : 10000, Describe : "主要产出御膳道具" 	},
-		{ Id : 104, OpenLevel : 5, UnlockPrice : [ "1_1180", "0_0000" ], GoldAddition : [ "0_1180" ], ItemGroup : [ "30001_1", "30002_1", "30003_1", "30004_1", "30005_1", "30006_1", "30007_1", "30008_1" ], ItemProb : 10000, Describe : "主要产出御品道具" 	},
-		{ Id : 105, OpenLevel : 6, UnlockPrice : [ "1_1180", "0_0000" ], GoldAddition : [ "0_1180" ], ItemGroup : [ "40001_1", "40002_1", "40003_1", "40004_1", "40005_1" ], ItemProb : 10000, Describe : "主要产出服装道具" 	}
+		{ Id : 300, OpenLevel : 1, UnlockPrice : [ "0_1" ], GoldAddition : [ "0_30" ], ItemGroup : [ "10001_1", "20001_1", "20002_1", "20003_1", "20004_1", "20005_1", "20006_1" ], ItemProb : 10000, Describe : "主要产出御膳道具" 	},
+		{ Id : 301, OpenLevel : 2, UnlockPrice : [ "1_60", "0_0000" ], GoldAddition : [ "0_60" ], ItemGroup : [ "30001_1", "30002_1", "30003_1", "30004_1", "30005_1", "30006_1", "30007_1", "30008_1" ], ItemProb : 10000, Describe : "主要产出御品道具" 	},
+		{ Id : 302, OpenLevel : 3, UnlockPrice : [ "1_100", "0_0000" ], GoldAddition : [ "0_100" ], ItemGroup : [ "40001_1", "40002_1", "40003_1", "40004_1", "40005_1" ], ItemProb : 10000, Describe : "主要产出服装道具" 	},
+		{ Id : 303, OpenLevel : 4, UnlockPrice : [ "1_120", "0_0000" ], GoldAddition : [ "0_120" ], ItemGroup : [ "10001_1", "20001_1", "20002_1", "20003_1", "20004_1", "20005_1", "20006_1" ], ItemProb : 10000, Describe : "主要产出御膳道具" 	},
+		{ Id : 304, OpenLevel : 5, UnlockPrice : [ "1_150", "0_0000" ], GoldAddition : [ "0_150" ], ItemGroup : [ "30001_1", "30002_1", "30003_1", "30004_1", "30005_1", "30006_1", "30007_1", "30008_1" ], ItemProb : 10000, Describe : "主要产出御品道具" 	},
+		{ Id : 305, OpenLevel : 6, UnlockPrice : [ "1_200", "0_0000" ], GoldAddition : [ "0_200" ], ItemGroup : [ "40001_1", "40002_1", "40003_1", "40004_1", "40005_1" ], ItemProb : 10000, Describe : "主要产出服装道具" 	},
+		{ Id : 200, OpenLevel : 1, UnlockPrice : [ "1_1000", "0_0000" ], GoldAddition : [ "0_1000" ], ItemGroup : [ "10001_1", "20001_1", "20002_1", "20003_1", "20004_1", "20005_1", "20006_1" ], ItemProb : 10000, Describe : "主要产出御膳道具" 	},
+		{ Id : 201, OpenLevel : 2, UnlockPrice : [ "1_3000", "0_0000" ], GoldAddition : [ "0_3000" ], ItemGroup : [ "30001_1", "30002_1", "30003_1", "30004_1", "30005_1", "30006_1", "30007_1", "30008_1" ], ItemProb : 10000, Describe : "主要产出御品道具" 	},
+		{ Id : 202, OpenLevel : 3, UnlockPrice : [ "1_6000", "0_0000" ], GoldAddition : [ "0_6000" ], ItemGroup : [ "40001_1", "40002_1", "40003_1", "40004_1", "40005_1" ], ItemProb : 10000, Describe : "主要产出服装道具" 	},
+		{ Id : 203, OpenLevel : 4, UnlockPrice : [ "2_1", "1_0000" ], GoldAddition : [ "1_1", "0_0000" ], ItemGroup : [ "10001_1", "20001_1", "20002_1", "20003_1", "20004_1", "20005_1", "20006_1" ], ItemProb : 10000, Describe : "主要产出御膳道具" 	},
+		{ Id : 204, OpenLevel : 5, UnlockPrice : [ "2_1", "1_4000" ], GoldAddition : [ "1_1", "0_4000" ], ItemGroup : [ "30001_1", "30002_1", "30003_1", "30004_1", "30005_1", "30006_1", "30007_1", "30008_1" ], ItemProb : 10000, Describe : "主要产出御品道具" 	},
+		{ Id : 205, OpenLevel : 6, UnlockPrice : [ "2_1", "1_9000" ], GoldAddition : [ "1_1", "0_9000" ], ItemGroup : [ "40001_1", "40002_1", "40003_1", "40004_1", "40005_1" ], ItemProb : 10000, Describe : "主要产出服装道具" 	},
+		{ Id : 100, OpenLevel : 1, UnlockPrice : [ "2_4", "1_0000" ], GoldAddition : [ "1_4", "0_0000" ], ItemGroup : [ "10001_1", "20001_1", "20002_1", "20003_1", "20004_1", "20005_1", "20006_1" ], ItemProb : 10000, Describe : "主要产出御膳道具" 	},
+		{ Id : 101, OpenLevel : 2, UnlockPrice : [ "2_18", "1_0000" ], GoldAddition : [ "1_18", "0_0000" ], ItemGroup : [ "30001_1", "30002_1", "30003_1", "30004_1", "30005_1", "30006_1", "30007_1", "30008_1" ], ItemProb : 10000, Describe : "主要产出御品道具" 	},
+		{ Id : 102, OpenLevel : 3, UnlockPrice : [ "2_48", "1_0000" ], GoldAddition : [ "1_48", "0_0000" ], ItemGroup : [ "40001_1", "40002_1", "40003_1", "40004_1", "40005_1" ], ItemProb : 10000, Describe : "主要产出服装道具" 	},
+		{ Id : 103, OpenLevel : 4, UnlockPrice : [ "2_78", "1_0000" ], GoldAddition : [ "1_78", "0_0000" ], ItemGroup : [ "10001_1", "20001_1", "20002_1", "20003_1", "20004_1", "20005_1", "20006_1" ], ItemProb : 10000, Describe : "主要产出御膳道具" 	},
+		{ Id : 104, OpenLevel : 5, UnlockPrice : [ "2_98", "1_0000" ], GoldAddition : [ "1_98", "0_0000" ], ItemGroup : [ "30001_1", "30002_1", "30003_1", "30004_1", "30005_1", "30006_1", "30007_1", "30008_1" ], ItemProb : 10000, Describe : "主要产出御品道具" 	},
+		{ Id : 105, OpenLevel : 6, UnlockPrice : [ "2_175", "1_0000" ], GoldAddition : [ "1_175", "0_0000" ], ItemGroup : [ "40001_1", "40002_1", "40003_1", "40004_1", "40005_1" ], ItemProb : 10000, Describe : "主要产出服装道具" 	}
 	]
 
 
