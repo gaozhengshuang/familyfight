359f952e8184bf8965f66eae5d0aef99ff8614af
Franky <liushuaigeq@163.com>
2018-10-17 10:47:12 +0800

    unlock first palace

--- server/gopath/src/gitee.com/jntse/minehero/server/gateserver/userpalace.go
+++ server/gopath/src/gitee.com/jntse/minehero/server/gateserver/userpalace.go
@@ -243,8 +243,7 @@ func (this *UserPalace) AddPalace(user *GateUser, id uint32) (palace *PalaceData
 	palace.id = id
 	palace.level = 1
 	palace.maids = make([]bool, 0)
-	palace.maids = append(palace.maids, true)
-	for i := 0;i < len(tmpl.Maids) - 1; i++ {
+	for i := 0;i < len(tmpl.Maids); i++ {
 		palace.maids = append(palace.maids, false)
 	}
 	mastertmpl := PalaceMgr().GetMasterConfig(id,1)
