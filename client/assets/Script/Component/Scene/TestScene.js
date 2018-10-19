480004a78d9d73d0d73d5637353daa6644c3982d
Franky <liushuaigeq@163.com>
2018-10-19 14:56:15 +0800

    update

--- client/assets/Script/Component/Scene/TestScene.js
+++ client/assets/Script/Component/Scene/TestScene.js
@@ -8,6 +8,23 @@ cc.Class({
     onLoad() {
     },
     start() {
+        let a = [
+            {
+                b: 1,
+                c: 2,
+            },
+            {
+                d: 3,
+            },
+            {
+                e: 4
+            }
+        ]
+        let x = {};
+        Game._.map(a, function (o) {
+            x = Game._.merge(x, o);
+        })
+        console.log(x);
     },
     update(dt) {
     },
