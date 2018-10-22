ea908d0942ea1c00e3b10f11b280558f5c77c0a4
hongdawei <84318436@qq.com>
2018-10-22 17:34:01 +0800

    MakeLove动画提交

--- client/assets/Script/Component/View/PalaceLoveView.js
+++ client/assets/Script/Component/View/PalaceLoveView.js
@@ -23,6 +23,7 @@ cc.Class({
 
     onEnable() {
         this.initNotification();
+        this.initView();
         this.updateView();
     },
 
@@ -35,24 +36,66 @@ cc.Class({
         this._topCurtainsPos = this.image_topCurtains.node.getPosition();
         this._leftCurtainsPos = this.image_leftCurtains.node.getPosition();
         this._rightCurtainsPos = this.image_rightCurtains.node.getPosition();
+
+        this._moveTime = 1.0;
+        this._delayTime = 2.0;
     },
 
     initNotification() {
         // Game.NotificationController.On(Game.Define.EVENT_KEY.PARTLVUP_ACK, this, this.updateView);
     },
 
-    updateView() {
-        //初始化帘子位置
+    initView() {
+        //初始化帘子
         this.image_topCurtains.node.setPosition(this._topCurtainsPos);
         this.image_leftCurtains.node.setPosition(this._leftCurtainsPos);
         this.image_rightCurtains.node.setPosition(this._rightCurtainsPos);
+        this.image_topCurtains.node.opacity = 0;
+        this.image_leftCurtains.node.opacity = 0;
+        this.image_rightCurtains.node.opacity = 0;
+    },
 
+    updateView() {
         this._data = Game.PalaceModel.GetPalaceDataById(Game.PalaceModel.GetCurPalaceId());
         if (this._data) {
         }
     },
 
     onGoLove() {
+        this.image_topCurtains.node.runAction(cc.sequence([
+            cc.spawn([
+                cc.moveTo(this._moveTime, this.image_topCurtains.node.x, this.image_topCurtains.node.y - this.image_topCurtains.node.height),
+                cc.fadeIn(this._moveTime)
+            ]),
+            cc.delayTime(this._delayTime),
+            cc.spawn([
+                cc.moveTo(this._moveTime, this._topCurtainsPos.x, this._topCurtainsPos.y),
+                cc.fadeOut(this._moveTime)
+            ]),
+        ]));
 
+        this.image_leftCurtains.node.runAction(cc.sequence([
+            cc.spawn([
+                cc.moveTo(this._moveTime, this.image_leftCurtains.node.x + this.image_leftCurtains.node.width, this.image_leftCurtains.node.y),
+                cc.fadeIn(this._moveTime)
+            ]),
+            cc.delayTime(this._delayTime),
+            cc.spawn([
+                cc.moveTo(this._moveTime, this._leftCurtainsPos.x, this._leftCurtainsPos.y),
+                cc.fadeOut(this._moveTime)
+            ]),
+        ]));
+
+        this.image_rightCurtains.node.runAction(cc.sequence([
+            cc.spawn([
+                cc.moveTo(this._moveTime, this.image_rightCurtains.node.x - this.image_rightCurtains.node.width, this.image_rightCurtains.node.y),
+                cc.fadeIn(this._moveTime)
+            ]),
+            cc.delayTime(this._delayTime),
+            cc.spawn([
+                cc.moveTo(this._moveTime, this._rightCurtainsPos.x, this._rightCurtainsPos.y),
+                cc.fadeOut(this._moveTime)
+            ]),
+        ]));
     }
 });
