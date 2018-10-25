cfce23ae16fed97ba6f77dbb5b1a890c8aa6c225
Franky <liushuaigeq@163.com>
2018-10-25 16:10:13 +0800

    update

--- client/assets/Script/Component/View/ShopView.js
+++ client/assets/Script/Component/View/ShopView.js
@@ -20,6 +20,7 @@ cc.Class({
 
     onDisable() {
         Game.NotificationController.Off(Game.Define.EVENT_KEY.MAID_UPDATESHOP, this, this.updateTableView);
+        Game.NotificationController.On(Game.Define.EVENT_KEY.USERINFO_UPDATEPASS, this, this.updateTableView);
     },
 
     onClose() {
@@ -29,13 +30,26 @@ cc.Class({
 
     initNotification() {
         Game.NotificationController.On(Game.Define.EVENT_KEY.MAID_UPDATESHOP, this, this.updateTableView);
+        Game.NotificationController.On(Game.Define.EVENT_KEY.USERINFO_UPDATEPASS, this, this.updateTableView);
     },
 
     initView() {
+        this.updateTableView();
     },
 
     updateTableView() {
-        let pass = Game.MaidModel.curPass;
+        let pass = Game.MaidModel.GetCurPass();
         let maidList = Game.MaidModel.GetMaidsByPass(pass);
+        for (let i = 0; i < this.shopCells.length; i++) {
+            let cell = this.shopCells[i];
+            let maid = maidList[i];
+            if (maid == null) {
+                cell.node.active = false;
+                continue;
+            }
+            cell.node.active = true;
+            let shop = Game._.find(Game.MaidModel._shopMaids, { id: maid.Id });
+            cell.init(maid, shop);
+        }
     },
 });
