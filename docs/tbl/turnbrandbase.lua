bf8b39685e41a770130accf4ce4abbd116cc3d6b
Franky <liushuaigeq@163.com>
2018-10-25 20:37:57 +0800

    update

--- docs/tbl/turnbrandbase.lua
+++ docs/tbl/turnbrandbase.lua
@@ -6,7 +6,7 @@ export var TurnBrand : table.ITurnBrandDefine[] = [
 		{ Id : 1, Name : "皇上", Head : "Image/Head/2_round", Reward : 11001, Dialogue : 0, Weight : 10 	},
 		{ Id : 2, Name : "皇后", Head : "Image/Head/1_round", Reward : 12001, Dialogue : 14, Weight : 2 	},
 		{ Id : 3, Name : "侍卫", Head : "Image/Head/5_round", Reward : 13001, Dialogue : 0, Weight : 20 	},
-		{ Id : 4, Name : "贵妃", Head : "Image/Head/4_round", Reward : 14001, Dialogue : 0, Weight : 20 	},
+		{ Id : 4, Name : "贵妃", Head : "Image/Head/4_round", Reward : 14001, Dialogue : 0, Weight : 8 	},
 		{ Id : 5, Name : "太监", Head : "Image/Head/3_round", Reward : 15001, Dialogue : 0, Weight : 40 	},
 		{ Id : 6, Name : "御医", Head : "Image/Head/6_round", Reward : 16001, Dialogue : 0, Weight : 20 	}
 	]
