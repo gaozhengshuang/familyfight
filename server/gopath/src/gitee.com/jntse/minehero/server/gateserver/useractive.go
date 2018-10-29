3da988c365c3969147f84cbaa264899a9e2cb165
Franky <liushuaigeq@163.com>
2018-10-29 16:48:57 +0800

    update

--- server/gopath/src/gitee.com/jntse/minehero/server/gateserver/useractive.go
+++ server/gopath/src/gitee.com/jntse/minehero/server/gateserver/useractive.go
@@ -1,114 +1,114 @@
 package main
+
 import (
-	"gitee.com/jntse/gotoolkit/log"
-	_"gitee.com/jntse/gotoolkit/eventqueue"
-	"gitee.com/jntse/minehero/pbmsg"
-	"gitee.com/jntse/minehero/server/tbl"
-	"gitee.com/jntse/minehero/server/tbl/excel"
-	"gitee.com/jntse/gotoolkit/util"
-	pb "github.com/golang/protobuf/proto"
 	"fmt"
-	"strings"
 	"strconv"
+	"strings"
+
+	_ "gitee.com/jntse/gotoolkit/eventqueue"
+	"gitee.com/jntse/gotoolkit/log"
+	"gitee.com/jntse/gotoolkit/util"
+	"gitee.com/jntse/minehero/server/tbl"
+	pb "github.com/golang/protobuf/proto"
 )
 
 // 获得奖励
-func (this *GateUser) AddReward(rtype uint32, rid uint32 ,rvalue uint32,rparam uint32,reason string,notify bool) (result uint32, value []string) { 
+func (this *GateUser) AddReward(rtype uint32, rid uint32, rvalue uint32, rparam uint32, reason string, notify bool) (result uint32, value []string) {
 	value = make([]string, 0)
 	switch rtype {
-		case 1:
-			//金币
-			goldrewardratio, find := tbl.TGoldRewardRatioBase.GoldRewardRatioById[rvalue]
-			if !find {
-				this.SendNotify("没有奖励金币的模板哟")
-				return 10, value
-			}
-			ratio := float64(0.0)
-			maxlevel := this.maid.GetMaxLevel()
-			for _, v := range goldrewardratio.RatioByLevel {
-				infos := strings.Split(v, "_")
-				if len(infos) >= 2 {
-					id, _ := strconv.ParseInt(infos[0],10,32)
-					if maxlevel == uint32(id) {
-						//找到了
-						ratio, _ = strconv.ParseFloat(infos[1], 64)
-						break
-					}
+	case 1:
+		//金币
+		goldrewardratio, find := tbl.TGoldRewardRatioBase.GoldRewardRatioById[rvalue]
+		if !find {
+			this.SendNotify("没有奖励金币的模板哟")
+			return 10, value
+		}
+		ratio := float64(0.0)
+		maxlevel := this.maid.GetMaxLevel()
+		for _, v := range goldrewardratio.RatioByLevel {
+			infos := strings.Split(v, "_")
+			if len(infos) >= 2 {
+				id, _ := strconv.ParseInt(infos[0], 10, 32)
+				if maxlevel == uint32(id) {
+					//找到了
+					ratio, _ = strconv.ParseFloat(infos[1], 64)
+					break
 				}
 			}
-			goldObj := this.maid.CalculateRewardPerSecond(this)
-			goldObj = this.TimesBigGold(goldObj, uint32(ratio))
-			goldObj = this.CarryBigGold(goldObj, this.MaxIndexBigGold(goldObj))
-			value = this.ParseBigGoldToArr(goldObj)
-			return 0, value
-		case 2:
-			//体力
-			this.AddPower(rvalue, reason, true,notify)
-			return 0, value 
-		case 3:
-			//侍女
-			maidconfg, find := tbl.TMaidLevelBase.TMaidLevelById[rid] 
-			if !find {
-				this.SendNotify("没有对应的侍女配置")
-				return 1, value
+		}
+		goldObj := this.maid.CalculateRewardPerSecond(this)
+		goldObj = this.TimesBigGold(goldObj, uint32(ratio))
+		goldObj = this.CarryBigGold(goldObj, this.MaxIndexBigGold(goldObj))
+		value = this.ParseBigGoldToArr(goldObj)
+		return 0, value
+	case 2:
+		//体力
+		this.AddPower(rvalue, reason, true, notify)
+		return 0, value
+	case 3:
+		//侍女
+		maidconfg, find := tbl.TMaidLevelBase.TMaidLevelById[rid]
+		if !find {
+			this.SendNotify("没有对应的侍女配置")
+			return 1, value
+		}
+		if reason != "开箱子" {
+			count := this.GetCountByLevel(uint32(maidconfg.Passlevels))
+			if count >= 20 {
+				this.SendNotify("该关卡侍女数量已达上限")
+				return 2, value
 			}
-			if reason != "开箱子" {
-				count := this.GetCountByLevel(uint32(maidconfg.Passlevels))
-				if count >= 20 {
-					this.SendNotify("该关卡侍女数量已达上限")
-					return 2, value
-				}
-			}
-			//可以获得了
-			maid := this.maid.AddMaid(this,rid,rvalue)
-			maidSend := &msg.GW2C_AckMaids{ Datas: make([]*msg.MaidData, 0) }
-			maidSend.Datas = append(maidSend.Datas, maid.PackBin())
-			maidSend.Maxid = pb.Uint32(this.maid.GetMaxId())
-			this.SendMsg(maidSend)
-			return 0, value
-		case 4:
-			//道具
-			this.AddItem(rid, rvalue, reason)
-			return 0, value
-		case 5:
-			//小游戏
-			return 0, value
-		case 6:
-			//小游戏的游戏币
-			//随机一个类型
-			gametype := uint32(util.RandBetween(int32(MiniGameCoinType_Start),int32(MiniGameCoinType_End) - 1))
-			this.currency.AddMiniGameCoin(gametype, rvalue, reason, notify)
-			return 0, value
-		default:
-			return 0, value
+		}
+		//可以获得了
+		maid := this.maid.AddMaid(this, rid, rvalue)
+		maidSend := &msg.GW2C_AckMaids{Datas: make([]*msg.MaidData, 0)}
+		maidSend.Datas = append(maidSend.Datas, maid.PackBin())
+		maidSend.Maxid = pb.Uint32(this.maid.GetMaxId())
+		this.SendMsg(maidSend)
+		return 0, value
+	case 4:
+		//道具
+		this.AddItem(rid, rvalue, reason)
+		return 0, value
+	case 5:
+		//小游戏
+		return 0, value
+	case 6:
+		//小游戏的游戏币
+		//随机一个类型
+		gametype := uint32(util.RandBetween(int32(MiniGameCoinType_Start), int32(MiniGameCoinType_End)-1))
+		this.currency.AddMiniGameCoin(gametype, rvalue, reason, notify)
+		return 0, value
+	default:
+		return 0, value
 	}
 }
 
 //翻牌子
-func (this *GateUser) TurnBrand(ids []uint32,level uint32) (result uint32, id uint32, drop *msg.RewardsData) {
-	// 体力够不够 
+func (this *GateUser) TurnBrand(ids []uint32, level uint32) (result uint32, id uint32, drop *msg.RewardsData) {
+	// 体力够不够
 	if this.GetPower() < 1 {
 		this.SendNotify("体力不足")
 		return 1, 0, nil
 	}
 	totalWeight := uint32(0)
-	brands := make([]*table.TurnBrandDefine,0)
-	for _,v := range ids {
+	brands := make([]*table.TurnBrandDefine, 0)
+	for _, v := range ids {
 		tmpl, find := tbl.TTurnBrandBase.TurnBrandById[v]
 		if !find {
 			log.Info("玩家[%d] 翻牌子 没有该牌子配置[%d]", this.Id(), v)
-			this.SendNotify(fmt.Sprintf("没有对应的牌子配置 [%d]",id))
+			this.SendNotify(fmt.Sprintf("没有对应的牌子配置 [%d]", id))
 			continue
 		}
 		brands = append(brands, tmpl)
 		totalWeight = totalWeight + tmpl.Weight
 	}
-	log.Info("总权重 %d",totalWeight)
+	log.Info("总权重 %d", totalWeight)
 	//随机吧
-	result = uint32(util.RandBetween(0, int32(totalWeight) - 1))
+	result = uint32(util.RandBetween(0, int32(totalWeight)-1))
 	var findbrand *table.TurnBrandDefine
 	for _, v := range brands {
-		log.Info("当前牌子 id: %d 权重: %d  当前权重 : %d",v.Id,v.Weight,result)
+		log.Info("当前牌子 id: %d 权重: %d  当前权重 : %d", v.Id, v.Weight, result)
 		if result < v.Weight {
 			//找到了
 			findbrand = v
@@ -121,48 +121,14 @@ func (this *GateUser) TurnBrand(ids []uint32,level uint32) (result uint32, id ui
 		return 2, 0, nil
 	}
 	//扣体力
-	this.RemovePower(1,"翻牌子消耗")
+	this.RemovePower(1, "翻牌子消耗")
 	golds, rewards := RewardMgr().DropToUser(this, findbrand.Reward, "翻牌子奖励", false, 0)
 	drop = RewardMgr().PackMsg(golds, rewards)
 	return 0, findbrand.Id, drop
 }
 
-//连连看
-func (this *GateUser) Linkup(score uint32) (result uint32, gold []string){
-	gold = make([]string, 0)
-	// 体力够不够 
-	if this.currency.GetMiniGameCoin(MiniGameCoinType_Linkup) < 1 {
-		this.SendNotify("游戏币不足")
-		return 1, gold
-	}
-	goldrewardratio, find := tbl.TGoldRewardRatioBase.GoldRewardRatioById[uint32(tbl.Common.LinkupGoldRewardRatio)]
-	if !find {
-		this.SendNotify("没有奖励金币的模板哟")
-		return 2, gold
-	}
-	maxlevel := this.maid.GetMaxLevel()
-	ratio := float64(0.0)
-	for _, v := range goldrewardratio.RatioByLevel {
-		infos := strings.Split(v, "_")
-		if len(infos) >= 2 {
-			id, _ := strconv.ParseInt(infos[0],10,32)
-			if maxlevel == uint32(id) {
-				//找到了
-				ratio, _ = strconv.ParseFloat(infos[1], 64)
-				break
-			}
-		}
-	}
-	goldObj := this.maid.CalculateRewardPerSecond(this)
-	goldObj = this.TimesBigGold(goldObj, uint32(ratio))
-	goldObj = this.TimesBigGold(goldObj, uint32(score))
-	goldObj = this.CarryBigGold(goldObj, this.MaxIndexBigGold(goldObj))
-	this.currency.RemoveMiniGameCoin(MiniGameCoinType_Linkup, 1,"十秒游戏消耗", true)
-	return 0, this.ParseBigGoldToArr(goldObj)
-}
-
 //箱子数据
-func (this *GateUser) LoadBox(bin *msg.Serialize){
+func (this *GateUser) LoadBox(bin *msg.Serialize) {
 	this.boxs = make(map[uint32]*BoxData)
 	boxs := bin.GetBoxs()
 	for _, v := range boxs {
@@ -176,23 +142,23 @@ func (this *GateUser) LoadBox(bin *msg.Serialize){
 }
 
 func (this *GateUser) PackBox(bin *msg.Serialize) {
-	bin.Boxs =  make([]*msg.BoxData,0) 
+	bin.Boxs = make([]*msg.BoxData, 0)
 	for _, box := range this.boxs {
 		bin.Boxs = append(bin.Boxs, box.PackBin())
 	}
 }
 
-func (this *GateUser) SynBox(){
-	send := &msg.GW2C_AckBoxData{ Box: make([]*msg.BoxData,0) }
+func (this *GateUser) SynBox() {
+	send := &msg.GW2C_AckBoxData{Box: make([]*msg.BoxData, 0)}
 	for _, box := range this.boxs {
 		send.Box = append(send.Box, box.PackBin())
 	}
 	this.SendMsg(send)
 }
 
-func (this *GateUser) GenerateBox(id uint32, num uint32, level uint32) *BoxData{
+func (this *GateUser) GenerateBox(id uint32, num uint32, level uint32) *BoxData {
 	count := this.GetCountByLevel(level)
-	if count + num > 20 {
+	if count+num > 20 {
 		return nil
 	}
 	//看看这关的侍女是不是到最大值了
@@ -227,7 +193,7 @@ func (this *GateUser) OpenBox(id uint32, num uint32) uint32 {
 		return 3
 	}
 	//开吧
-	result, _ := this.AddReward(boxtmpl.Type, boxtmpl.RewardId, boxtmpl.Value * num,0, "开箱子", true)
+	result, _ := this.AddReward(boxtmpl.Type, boxtmpl.RewardId, boxtmpl.Value*num, 0, "开箱子", true)
 	if result != 0 {
 		return result
 	} else {
@@ -256,7 +222,7 @@ func (this *GateUser) TickBox(now uint64) {
 			} else {
 				//找到了 看看时间差
 				//看看能不能掉
-				if now > boxData.generatetime + uint64(v.Interval) {
+				if now > boxData.generatetime+uint64(v.Interval) {
 					boxData = this.GenerateBox(v.Id, 1, v.Level)
 					if boxData != nil {
 						generated = true
@@ -269,11 +235,33 @@ func (this *GateUser) TickBox(now uint64) {
 		this.SynBox()
 	}
 }
+
+//连连看
+func (this *GateUser) Linkup(score uint32) (result uint32, gold []string) {
+	gold = make([]string, 0)
+	// 体力够不够
+	if this.currency.GetMiniGameCoin(MiniGameCoinType_Linkup) < 1 {
+		this.SendNotify("游戏币不足")
+		return 1, gold
+	}
+	rewardid := uint32(0)
+	hit := (score == 2)
+	if hit {
+		rewardid = uint32(tbl.Common.LinkupWinReward)
+	} else {
+		rewardid = uint32(tbl.Common.LinkupLoseReward)
+	}
+	golds, _ := RewardMgr().DropToUser(this, rewardid, "连连看奖励", true, 0)
+	// this.SendRewardNotify(golds, rewards)
+	this.currency.RemoveMiniGameCoin(MiniGameCoinType_Linkup, 1, "连连看游戏消耗", true)
+	return 0, golds
+}
+
 //十秒小游戏
 func (this *GateUser) TenSecond(hit bool) (result uint32, gold []string, items []*msg.PairNumItem) {
 	gold = make([]string, 0)
 	items = make([]*msg.PairNumItem, 0)
-	// 体力够不够 
+	// 体力够不够
 	if this.currency.GetMiniGameCoin(MiniGameCoinType_TenSecond) < 1 {
 		this.SendNotify("游戏币不足")
 		return 1, gold, items
@@ -284,7 +272,7 @@ func (this *GateUser) TenSecond(hit bool) (result uint32, gold []string, items [
 		for _, v := range tbl.Common.TenSecondWinReward.Item {
 			if len(v) >= 2 {
 				this.AddReward(4, uint32(v[0]), uint32(v[1]), 0, "十秒游戏奖励", true)
-				items = append(items, &msg.PairNumItem{ Itemid: pb.Uint32( uint32(v[0])), Num: pb.Uint64(uint64(v[1])) })
+				items = append(items, &msg.PairNumItem{Itemid: pb.Uint32(uint32(v[0])), Num: pb.Uint64(uint64(v[1]))})
 			}
 		}
 		goldObj := this.maid.CalculateRewardPerSecond(this)
@@ -299,13 +287,14 @@ func (this *GateUser) TenSecond(hit bool) (result uint32, gold []string, items [
 		gold = this.ParseBigGoldToArr(goldObj)
 	}
 	//扣体力
-	this.currency.RemoveMiniGameCoin(MiniGameCoinType_TenSecond, 1,"十秒游戏消耗", true)
+	this.currency.RemoveMiniGameCoin(MiniGameCoinType_TenSecond, 1, "十秒游戏消耗", true)
 	return 0, gold, items
 }
+
 //踢屁股
-func (this *GateUser) KickAss (hit bool) (result uint32, gold []string) {
+func (this *GateUser) KickAss(hit bool) (result uint32, gold []string) {
 	gold = make([]string, 0)
-	// 体力够不够 
+	// 体力够不够
 	if this.currency.GetMiniGameCoin(MiniGameCoinType_KickAss) < 1 {
 		this.SendNotify("游戏币不足")
 		return 1, gold
@@ -325,7 +314,7 @@ func (this *GateUser) KickAss (hit bool) (result uint32, gold []string) {
 		gold = this.ParseBigGoldToArr(goldObj)
 	}
 	//扣体力
-	this.currency.RemoveMiniGameCoin(MiniGameCoinType_KickAss, 1,"踢屁股消耗", true)
+	this.currency.RemoveMiniGameCoin(MiniGameCoinType_KickAss, 1, "踢屁股消耗", true)
 	return 0, gold
 }
 
@@ -369,7 +358,7 @@ func (this *GateUser) AttackPalace(id uint64) uint32 {
 		}
 		user := UserMgr().FindById(id)
 		if user != nil {
-			send := &msg.GW2C_PushActiveRecord{ Records: make([]string, 0) }
+			send := &msg.GW2C_PushActiveRecord{Records: make([]string, 0)}
 			send.Records = append(send.Records, record)
 			user.SendMsg(send)
 		}
@@ -380,10 +369,10 @@ func (this *GateUser) AttackPalace(id uint64) uint32 {
 func (this *GateUser) SynAttackPalaceRecords() {
 	key := fmt.Sprintf("activerecord_%d", this.Id())
 	rlist, err := Redis().LRange(key, 0, -1).Result()
-	send := &msg.GW2C_PushActiveRecord{ Records: make([]string, 0) }
+	send := &msg.GW2C_PushActiveRecord{Records: make([]string, 0)}
 	if err != nil {
 		log.Error("加载互动记录失败 id %d ，err: %s", this.Id(), err)
-	}else{
+	} else {
 		send.Records = rlist
 	}
 	this.SendMsg(send)
@@ -392,7 +381,7 @@ func (this *GateUser) SynAttackPalaceRecords() {
 //猜皇帝
 func (this *GateUser) ReqGuessKingData() *msg.GW2C_AckGuessKingData {
 	robots := RobotMgr().RandomRobot(3)
-	send := &msg.GW2C_AckGuessKingData{ Data: make([]*msg.RobotPalaceData, 0) }
+	send := &msg.GW2C_AckGuessKingData{Data: make([]*msg.RobotPalaceData, 0)}
 	for _, robot := range robots {
 		data := &msg.RobotPalaceData{}
 		data.Id = pb.Uint64(robot.id)
@@ -408,9 +397,9 @@ func (this *GateUser) ReqGuessKingData() *msg.GW2C_AckGuessKingData {
 	return send
 }
 
-func (this *GateUser) GuessKing(id uint64, index uint32)(result uint32, randkey uint32){
+func (this *GateUser) GuessKing(id uint64, index uint32) (result uint32, randkey uint32) {
 	rewardid := uint32(0)
-	randkey = uint32(util.RandBetween(0, int32(tbl.Common.GuessKingCount) - 1))
+	randkey = uint32(util.RandBetween(0, int32(tbl.Common.GuessKingCount)-1))
 	hit := (index == randkey)
 	if hit {
 		rewardid = uint32(tbl.Common.GuessKingWinReward)
@@ -437,16 +426,17 @@ func (this *GateUser) GuessKing(id uint64, index uint32)(result uint32, randkey 
 		}
 		user := UserMgr().FindById(id)
 		if user != nil {
-			send := &msg.GW2C_PushActiveRecord{ Records: make([]string, 0) }
+			send := &msg.GW2C_PushActiveRecord{Records: make([]string, 0)}
 			send.Records = append(send.Records, record)
 			user.SendMsg(send)
 		}
 	}
 	return 0, randkey
 }
+
 //临幸
 func (this *GateUser) ReqLuckily(palaceid uint32) uint32 {
-	// 体力够不够 
+	// 体力够不够
 	if this.currency.GetMiniGameCoin(MiniGameCoinType_Luckily) < 1 {
 		this.SendNotify("游戏币不足")
 		return 1
@@ -454,13 +444,13 @@ func (this *GateUser) ReqLuckily(palaceid uint32) uint32 {
 	golds, rewards := RewardMgr().DropToUser(this, uint32(tbl.Common.LuckilyReward), "临幸奖励", true, palaceid)
 	this.SendRewardNotify(golds, rewards)
 	//扣体力
-	this.currency.RemoveMiniGameCoin(MiniGameCoinType_Luckily, 1,"临幸消耗", true)
+	this.currency.RemoveMiniGameCoin(MiniGameCoinType_Luckily, 1, "临幸消耗", true)
 	return 0
 }
 
 //约会
 func (this *GateUser) ReqTryst(palaceid uint32, key uint32) uint32 {
-	// 体力够不够 
+	// 体力够不够
 	if this.currency.GetMiniGameCoin(MiniGameCoinType_Tryst) < 1 {
 		this.SendNotify("游戏币不足")
 		return 1
@@ -469,6 +459,6 @@ func (this *GateUser) ReqTryst(palaceid uint32, key uint32) uint32 {
 	golds, rewards := RewardMgr().DropToUser(this, rewardid, "约会奖励", true, palaceid)
 	this.SendRewardNotify(golds, rewards)
 	//扣体力
-	this.currency.RemoveMiniGameCoin(MiniGameCoinType_Tryst, 1,"约会消耗", true)
+	this.currency.RemoveMiniGameCoin(MiniGameCoinType_Tryst, 1, "约会消耗", true)
 	return 0
 }
